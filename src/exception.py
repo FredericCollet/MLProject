558d99bd37c351b73d9860492b965d15c9977161
gitfred12 <fred.collet@gmail.com>
2024-09-30 12:04:07 -0600

    le model qui va pas bien ( oh /split)

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 
 # manage error messages 
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -20,6 +20,7 @@ class CustomException(Exception):
         return self.error_message
     
 
+    
 
    
         
